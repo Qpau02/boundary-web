2b2bf971633758fefbf7e382b7985c963ea67334
qpau02 <qpau02@gmail.com>
2026-04-01 11:42:27 +0200

    generate same sized canvas on top of the puzzle

--- js/dependencies/puzzles.js
+++ js/dependencies/puzzles.js
@@ -13,3 +13,65 @@ export const testing = {
         verified: "Qpau02",
     },
 };
+
+export const scoringExampleOne = {
+    name: "Scoring Example One",
+    grid: [
+        ["blank", "wall", "blank", "blank"],
+        ["blank", "blank", "blank", "wall"],
+        ["blank", "blank", "blank", "blank"],
+        ["blank", "blank", "blank", "wall"],
+    ],
+    max_vertices: 4,
+    goal: 90,
+    info: {
+        creator: "the_upgrater",
+        verified: "the_upgrater",
+    },
+};
+export const scoringExampleTwo = {
+    name: "Scoring Example Two",
+    grid: [
+        ["wall", "blank", "wall", "wall"],
+        ["blank", "blank", "blank", "blank"],
+        ["wall", "blank", "blank", "wall"],
+        ["blank", "blank", "wall", "blank"],
+    ],
+    max_vertices: 3,
+    goal: 40,
+    info: {
+        creator: "the_upgrater",
+        verified: "the_upgrater",
+    },
+};
+export const scoringExampleThree = {
+    name: "Scoring Example Three",
+    grid: [
+        ["wall", "blank", "blank", "blank"],
+        ["wall", "blank", "blank", "blank"],
+        ["blank", "blank", "blank", "wall"],
+        ["wall", "blank", "blank", "wall"],
+    ],
+    max_vertices: 3,
+    goal: 50,
+    info: {
+        creator: "the_upgrater",
+        verified: "the_upgrater",
+    },
+};
+export const triangulation = {
+    name: "Triangulation",
+    grid: [
+        ["blank", "blank", "metal", "wall"],
+        ["blank", "blank", "blank", "wall"],
+        ["capture", "blank", "capture", "blank"],
+        ["glass", "blank", "blank", "mine"],
+        ["gem", "wall", "blank", "wall"],
+    ],
+    max_vertices: 3,
+    goal: 70,
+    info: {
+        creator: "Codex",
+        verified: "?",
+    },
+};
--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@ import { puzzleBuild } from "./dependencies/library.js";
 import * as puzzles from "./dependencies/puzzles.js";
 
 window.onload = () => {
+    const canvas = document.getElementById("canvas");
     const grid = document.getElementById("grid");
     const text = document.getElementsByClassName("text");
 
     puzzleBuild(puzzles.testing, text, grid);
+
+    canvas.style.width = `${grid.offsetWidth}px`;
+    canvas.style.height = `${grid.offsetHeight}px`;
 };
